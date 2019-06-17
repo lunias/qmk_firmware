e2b4830c0b9d30c11fd36368342965843cb87fcc
Ethan Anderson <ethanaa@gmail.com>
2019-06-17 18:36:57 -0500

    twinkle engaged

--- keyboards/tokyo60/keymaps/lunias/rules.mk
+++ keyboards/tokyo60/keymaps/lunias/rules.mk
@@ -11,12 +11,13 @@ MOUSEKEY_ENABLE							= no  # Mouse keys(+4700)
 NKRO_ENABLE									= yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 RGBLIGHT_ENABLE							= yes # Enable RGB light
 RGBLIGHT_STARTUP_ANIMATION	= yes
-RGBLIGHT_TWINKLE						= no
+RGBLIGHT_TWINKLE						= yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE						= no  # Breathing sleep LED during USB suspend
 SPACE_CADET_ENABLE					= yes
 SWAP_HANDS_ENABLE						= no
 TAP_DANCE_ENABLE						= no
 UNICODE_ENABLE							= yes  # Unicode
+
 
 BOOTLOADER									= atmel-dfu
