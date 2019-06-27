9a3dc22f63ed18b0b4d1b5e43d9f036b4e3f475a
Ethan Anderson <eanderson@vivial.net>
2019-06-27 10:03:30 -0500

    back to MO() and enble RGBLIGHT_SLEEP

--- keyboards/tokyo60/keymaps/lunias/rules.mk
+++ keyboards/tokyo60/keymaps/lunias/rules.mk
@@ -12,6 +12,7 @@ NKRO_ENABLE									= yes    # USB Nkey Rollover - if this doesn't work, see her
 RGBLIGHT_ENABLE							= yes    # Enable RGB light
 RGBLIGHT_STARTUP_ANIMATION	= yes
 RGBLIGHT_TWINKLE						= yes
+RGBLIGHT_SLEEP							= yes
 																		 # Do not enable SLEEP_LED_ENABLE it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE						= no     # Breathing sleep LED during USB suspend
 SPACE_CADET_ENABLE					= yes
